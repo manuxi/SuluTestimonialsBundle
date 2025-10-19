0f2ad0c2aea3842b33f817e76689af580e9a859e
Manuel Bertrams <manuel@bertrams-media.de>
2025-10-19 23:03:06 +0200

    category fix

--- src/Entity/TestimonialExcerptTranslation.php
+++ src/Entity/TestimonialExcerptTranslation.php
@@ -6,29 +6,47 @@ namespace Manuxi\SuluTestimonialsBundle\Entity;
 
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Doctrine\ORM\Mapping\InverseJoinColumn;
+use Doctrine\ORM\Mapping\JoinColumn;
 use Doctrine\ORM\Mapping\JoinTable;
+use Doctrine\ORM\Mapping\ManyToMany;
 use Manuxi\SuluSharedToolsBundle\Entity\Abstracts\Entity\AbstractExcerptTranslation;
 use Manuxi\SuluSharedToolsBundle\Entity\Interfaces\ExcerptTranslationInterface;
 use Manuxi\SuluTestimonialsBundle\Repository\TestimonialExcerptTranslationRepository;
+use Sulu\Bundle\CategoryBundle\Entity\Category;
+use Sulu\Bundle\MediaBundle\Entity\MediaInterface;
+use Sulu\Bundle\TagBundle\Tag\TagInterface;
 
 #[ORM\Entity(repositoryClass: TestimonialExcerptTranslationRepository::class)]
 #[ORM\Table(name: 'app_testimonial_excerpt_translation')]
 class TestimonialExcerptTranslation extends AbstractExcerptTranslation implements ExcerptTranslationInterface
 {
+    #[ManyToMany(targetEntity: Category::class)]
     #[JoinTable(name: 'app_testimonial_excerpt_categories')]
+    #[JoinColumn(name: 'excerpt_id', referencedColumnName: 'id')]
+    #[InverseJoinColumn(name: 'category_id', referencedColumnName: 'id')]
     protected ?Collection $categories = null;
 
+    #[ManyToMany(targetEntity: TagInterface::class)]
     #[JoinTable(name: 'app_testimonial_excerpt_tags')]
+    #[JoinColumn(name: 'excerpt_id', referencedColumnName: 'id')]
+    #[InverseJoinColumn(name: 'tag_id', referencedColumnName: 'id')]
     protected ?Collection $tags = null;
 
+    #[ManyToMany(targetEntity: MediaInterface::class)]
     #[JoinTable(name: 'app_testimonial_excerpt_icons')]
+    #[JoinColumn(name: 'excerpt_id', referencedColumnName: 'id')]
+    #[InverseJoinColumn(name: 'icon_id', referencedColumnName: 'id')]
     protected ?Collection $icons = null;
 
+    #[ManyToMany(targetEntity: MediaInterface::class)]
     #[JoinTable(name: 'app_testimonial_excerpt_images')]
+    #[JoinColumn(name: 'excerpt_id', referencedColumnName: 'id')]
+    #[InverseJoinColumn(name: 'image_id', referencedColumnName: 'id')]
     protected ?Collection $images = null;
 
     #[ORM\ManyToOne(targetEntity: TestimonialExcerpt::class, inversedBy: 'translations')]
-    #[ORM\JoinColumn(nullable: false)]
+    #[JoinColumn(nullable: false)]
     private TestimonialExcerpt $testimonialExcerpt;
 
     public function __construct(TestimonialExcerpt $testimonialExcerpt, string $locale)
