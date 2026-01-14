41195799979ff2cb691df1075f856aecaf37a46d
Manuel Bertrams <manuel@bertrams-media.de>
2026-01-15 00:49:55 +0100

    Eager Loading fix

--- src/Repository/TestimonialRepository.php
+++ src/Repository/TestimonialRepository.php
@@ -207,32 +207,19 @@ class TestimonialRepository extends ServiceEntityRepository
     ): void {
         $locale = $filters['locale'] ?? null;
         $stage = $filters['stage'] ?? DimensionContentInterface::STAGE_DRAFT;
-        $version = $filters['version'] ?? DimensionContentInterface::CURRENT_VERSION;
 
-        $queryBuilder->leftJoin(
-            'testimonial.dimensionContents',
-            'dimensionContent',
-            'WITH',
-            'dimensionContent.stage = :stage AND dimensionContent.version = :version'
-            . ($locale ? ' AND dimensionContent.locale = :locale' : '')
-        );
+        $queryBuilder->leftJoin('testimonial.dimensionContents', 'dimensionContent');
 
-        $queryBuilder->setParameter('stage', $stage);
-        $queryBuilder->setParameter('version', $version);
-
-        if ($locale) {
-            $queryBuilder->setParameter('locale', $locale);
-        }
-
-        $queryBuilder->addSelect('dimensionContent');
-
-        if (!empty($selects)) {
+        $normalizedSelects = $this->normalizeSelects($selects);
+        if (!empty($normalizedSelects)) {
             $this->dimensionContentQueryEnhancer->addSelects(
                 $queryBuilder,
                 TestimonialDimensionContent::class,
                 ['locale' => $locale, 'stage' => $stage],
-                $selects
+                $normalizedSelects
             );
+        } else {
+            $queryBuilder->addSelect('dimensionContent');
         }
     }
 
